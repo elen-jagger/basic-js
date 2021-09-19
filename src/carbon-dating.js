6253e5efa23b3c0571d8b7c35db8618e8e7d5173
Elena Leusik <elena.leusik@gmail.com>
2021-09-19 17:37:23 +0300

    add carbon-dating and count-cats functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity != "string" || !Number(sampleActivity) || Number(sampleActivity) <= 0 || Number(sampleActivity) > 15) { return false; }
+  else {
+    return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / 693 * 1000 * HALF_LIFE_PERIOD);
+  } 
+  
+    
 }
