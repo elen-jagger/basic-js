092722ad7e6b63780bfc5d513e168c5212bc867b
Elena Leusik <elena.leusik@gmail.com>
2021-09-20 22:40:23 +0300

    feat; add transform-array and what season functions

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  } else {
+    let timestamp = Date.parse(date);
+
+    if(timestamp) {
+      let givenDate = new Date(timestamp);
+      let month = givenDate.getMonth();
+  
+      switch(month) {
+        case 0:
+        case 1:
+        case 11:
+          return 'winter';
+        case 2:
+        case 3:
+        case 4:
+          return 'spring';
+        case 5:
+        case 6:
+        case 7:
+          return 'summer';
+        case 8:
+        case 9:
+        case 10:
+          return 'autumn ';
+      }
+
+    } else {
+      throw new Error('Invalid date!');
+    }
+   
+  }
+  
 }
